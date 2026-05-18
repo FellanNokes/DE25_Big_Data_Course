11d1c7cb0a8bea2bdc19a51bfdcf41265fe00408
FellanNokes <felix.l.kjellberg@gmail.com>
2026-05-18 07:17:28 +0000

    some eda and logs

--- 05_bronze/explorations/EDA.py
+++ 05_bronze/explorations/EDA.py
@@ -13,3 +13,25 @@ spark.sql(f"LIST '{VOLUME_PATH}'").display()
 spark.sql(f"LIST '{VOLUME_PATH}/data'").display()
 spark.sql(f"LIST '{VOLUME_PATH}/logs'").display()
 spark.sql(f"LIST '{VOLUME_PATH}/metadata'").display()
+
+# COMMAND ----------
+
+df = spark.sql("SELECT * FROM supply_chain.bronze.raw_supply_chain")
+
+df.display()
+
+# COMMAND ----------
+
+df_amount_order_status = spark.sql("""
+                                   SELECT
+                                       COUNT(*) AS amount,
+                                       Order_Status
+                                   FROM supply_chain.bronze.raw_supply_chain
+                                   GROUP BY Order_Status
+                                   """)
+
+display(df_amount_order_status)
+
+# COMMAND ----------
+
+df_amount_order_status.plot(kind='bar', x='Order_Status', y='amount')
